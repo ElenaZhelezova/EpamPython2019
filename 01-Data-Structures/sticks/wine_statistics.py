86d80893e46930a2ce5d53447605d4a03dfc28a0
ElenaZhelezova <elena.zhelezova@gmail.com>
2019-06-22 21:18:50 +0300

    parser

--- 01-Data-Structures/sticks/wine_statistics.py
+++ 01-Data-Structures/sticks/wine_statistics.py
@@ -1,5 +1,21 @@
 import json
+import re
 from collections import Counter
+
+
+pattern_line = re.compile(r"^\{'points': \'?(?P<points>[^\'\,]+)\'?, "
+                          r"'title': \'?[^\'\,]+\'?, "
+                          r"'description': \"[^\"]+\", "
+                          r"'taster_name': \'?(?P<taster_name>[^\'\,]+)\'?, "
+                          r"'taster_twitter_handle': \'?[^\'\,]+\'?, "
+                          r"'price': \'?(?P<price>[^\'\,]+)\'?, "
+                          r"'designation': \'?[^\'\,]+\'?, "
+                          r"'variety': \'?(?P<variety>[^\'\,]+)\'?, "
+                          r"'region_1': \'?(?P<region_1>[^\'\,]+)\'?, "
+                          r"'region_2': \'?(?P<region_2>[^\'\,]+)\'?, "
+                          r"'province': \'?[^\'\,]+\'?, "
+                          r"'country': \'?(?P<country>[^\'\,]+)\'?, "
+                          r"'winery': \'?[^\,\']+\'?\}$")
 
 
 def get_whole_data(data_1, data_2):
@@ -14,60 +30,56 @@ def get_whole_data(data_1, data_2):
     dumping_data = [dict(item) for item in unique_items_set]
     dumping_data.sort(key=lambda item: (0-(item['price'] or 0), item['variety'] or 'z'))
 
-    with open('winedata_full.json', 'w') as d:
+    full_winedata = 'winedata_full.json'
+    with open(full_winedata, 'w') as d:
         json.dump(dumping_data, d, ensure_ascii=False, indent='\t')
 
-    return dumping_data
+    return full_winedata
 
 
-def parse_data(data):
+def create_report_wine_dict(data):
+    wine_dict = {}
+
     for item in data:
-        points = item['points']
-        price = item['price']
-        variety = item['variety']
-        region1 = item['region_1']
-        region2 = item['region_2']
-        country = item['country']
-        yield points, price, variety, region1, region2, country
+        if item["variety"] and item["variety"] != "None" and item["variety"] != 'None':
+            if item["variety"] not in wine_dict:
+                wine_dict[item["variety"]] = {'points': [],
+                                              'price': [],
+                                              'region': [],
+                                              'country': []}
+            if item["points"] is not None:
+                wine_dict[item["variety"]]['points'].append(item["points"])
+            if item["price"] is not None:
+                wine_dict[item["variety"]]['price'].append(item["price"])
+            if item["region_1"] is not None and item["region_1"] != "None" and item["region_1"] != 'None':
+                wine_dict[item["variety"]]['region'].append(item["region_1"])
+            if item["region_2"] is not None and item["region_2"] != "None" and item["region_2"] != 'None':
+                wine_dict[item["variety"]]['region'].append(item["region_2"])
+            if item["country"] is not None and item["country"] != "None" and item["country"] != 'None':
+                wine_dict[item["variety"]]['country'].append(item["country"])
+
+    return wine_dict
 
 
-def create_report_wine_dict(dict_in, *args):
-    points, price, variety, reg1, reg2, country = args
+def create_report_country_dict(data):
+    country_dict = {}
 
-    if variety not in dict_in:
-        dict_in[variety] = {'points': [],
-                            'price': [],
-                            'region': [],
-                            'country': []}
+    for item in data:
+        if item["country"] and item["country"] != "None" and item["country"] != 'None':
+            if item["country"] not in country_dict:
+                country_dict[item["country"]] = {'points': [],
+                                                 'price': []}
 
-    if points is not None:
-        dict_in[variety]['points'].append(int(points))
-    if price is not None:
-        dict_in[variety]['price'].append(price)
-    if reg1 is not None:
-        dict_in[variety]['region'].append(reg1)
-    if reg2 is not None:
-        dict_in[variety]['region'].append(reg2)
-    if country is not None:
-        dict_in[variety]['country'].append(country)
+            if item["points"] is not None:
+                country_dict[item["country"]]['points'].append(item["points"])
+            if item["price"] is not None:
+                country_dict[item["country"]]['price'].append(item["price"])
 
-    return dict_in
+    return country_dict
 
 
-def create_report_country_dict(dict_in, points, price, country):
-    if country not in dict_in:
-        dict_in[country] = {'points': [],
-                            'price': []}
-
-    if points is not None:
-        dict_in[country]['points'].append(int(points))
-    if price is not None:
-        dict_in[country]['price'].append(float(price))
-
-    return dict_in
-
-
-def get_wine_stat(dict_data):
+def get_wine_stat(data):
+    dict_data = create_report_wine_dict(data)
     wine_stat_dict = {}
     wines = ['Gewürztraminer', 'Riesling', 'Merlot', 'Madera', 'Tempranillo', 'Red Blend']
 
@@ -85,7 +97,8 @@ def get_wine_stat(dict_data):
     return wine_stat_dict
 
 
-def get_country_stat(dict_data):
+def get_country_stat(data):
+    dict_data = create_report_country_dict(data)
     for item in dict_data:
         dict_data[item]['avg_price'] = (sum(dict_data[item]['price'])/len(dict_data[item]['price'])
                                         if len(dict_data[item]['price']) > 0 else 0)
@@ -116,42 +129,68 @@ def get_country_stat(dict_data):
     return country_stat_dict
 
 
-def get_stat(data):
-    report_wine_dict = {}
-    report_country_dict = {}
-    records = parse_data(data)
+def parse_item(record):
+    match_line = pattern_line.match(record)
+    if match_line:
+        try:
+            points = int(match_line.groupdict()['points'].strip(''""))
+        except ValueError:
+            points = None
+        taster_name = match_line.groupdict()['taster_name'].strip(''"")
+        try:
+            price = float(match_line.groupdict()['price'].strip(''""))
+        except ValueError:
+            price = None
+        variety = match_line.groupdict()['variety'].strip(''"")
+        region_1 = match_line.groupdict()['region_1'].strip(''"")
+        region_2 = match_line.groupdict()['region_2'].strip(''"")
+        country = match_line.groupdict()['country'].strip(''"")
+        return points, taster_name, price, variety, region_1, region_2, country
+    return
+
+
+def read_data_file(file_name):
+    with open(file_name) as f:
+        for item in json.load(f):
+            record_item = parse_item(str(item))
+            if record_item:
+                yield record_item
+
+
+def get_statistics(data_file):
+    stat_data = []
+    records = read_data_file(data_file)
     for i in records:
-        report_wine_dict = create_report_wine_dict(report_wine_dict, *i)
-        report_country_dict = create_report_country_dict(report_country_dict, i[0], i[1], i[5])
+        stat_data.append({"points": i[0], "taster_name": i[1], "price": i[2], "variety": i[3],
+                          "region_1": i[4], "region_2": i[5], "country": i[6]})
 
-    wine_stat = get_wine_stat(report_wine_dict)
-    country_stat = get_country_stat(report_country_dict)
-    return wine_stat, country_stat
+    stat_data.sort(key=lambda item: item['price'] or 0, reverse=True)
 
-
-def get_statistics(data):
-    most_expensive_wine_set = set([x['variety'] for x in data if x['price'] == data[0]['price']])
+    most_expensive_wine_set = set([x['variety'] for x in stat_data if x['price'] == stat_data[0]['price']])
     most_expensive_wine = [item for item in most_expensive_wine_set]
 
-    data_without_null_price = list(filter(lambda item: item['price'], data))
+    data_without_null_price = list(filter(lambda item: item['price'], stat_data))
 
     cheapest_wine_set = set([x['variety'] for x in data_without_null_price
                              if x['price'] == data_without_null_price[-1]['price']])
     cheapest_wine = [item for item in cheapest_wine_set]
 
-    data.sort(key=lambda item: item['points'] or 0)
+    stat_data.sort(key=lambda item: item['points'] or 0)
 
-    highest_score_set = set([x['variety'] for x in data if x['points'] == data[-1]['points']])
+    highest_score_set = set([x['variety'] for x in stat_data if x['points'] == stat_data[-1]['points']])
     highest_score = [item for item in highest_score_set]
 
-    lowest_score_set = set([x['variety'] for x in data if x['points'] == data[0]['points']])
+    data_without_null_score = list(filter(lambda item: item['points'], stat_data))
+
+    lowest_score_set = set([x['variety'] for x in data_without_null_score if x['points'] == stat_data[0]['points']])
     lowest_score = [item for item in lowest_score_set]
 
     most_active_commentator = Counter(
-        item['taster_name'] for item in data if item['taster_name'] is not None).most_common(1)[0][0]
+        item['taster_name'] for item in stat_data
+        if item['taster_name'] and item['taster_name'] != "None" and item['taster_name'] != 'None').most_common(1)[0][0]
 
-    wine_stat_dict = get_stat(data)[0]
-    country_stat_dict = get_stat(data)[1]
+    wine_stat_dict = get_wine_stat(stat_data)
+    country_stat_dict = get_country_stat(stat_data)
 
     statictics = {"statistics": {
                     "wine": wine_stat_dict,
